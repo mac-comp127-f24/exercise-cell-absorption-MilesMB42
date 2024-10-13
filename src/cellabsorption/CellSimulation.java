4783417ced1d45c972201cc5411be8a6e4f3c7d5
MilesMB42 <mmccart3@macalester.edu>
2024-10-13 03:09:51 -0400

    Finished task 2

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -5,6 +5,8 @@ import edu.macalester.graphics.Ellipse;
 import edu.macalester.graphics.Point;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @SuppressWarnings("SameParameterValue")
@@ -15,7 +17,8 @@ public class CellSimulation {
 
     private CanvasWindow canvas;
     private Random rand = new Random();
-    private Cell cell;
+    private List<Cell> cells;
+    
 
     public static void main(String[] args) {
         new CellSimulation();
@@ -28,23 +31,30 @@ public class CellSimulation {
         //noinspection InfiniteLoopStatement
         while (true) {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            cell.moveAround(canvasCenter);
-            cell.grow(0.02);
-
+            
+            for (Cell cell : cells) {
+                cell.moveAround(canvasCenter);
+                cell.grow(0.02);
+            }
             canvas.draw();
             canvas.pause(10);
         }
     }
 
     private void populateCells() {
-        double size = rand.nextInt(5) + 2;
-        cell = new Cell();
-        cell.Cell(
-            rand.nextDouble() * (canvas.getWidth() - size),
+        cells = new ArrayList<>();
+        for (int i = 0; i < 200; i++){
+            Cell newCell = new Cell();
+            double size = rand.nextInt(5) + 2;
+            newCell.Cell(rand.nextDouble() * (canvas.getWidth() - size),
             rand.nextDouble() * (canvas.getWidth() - size),
             size,
             Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        canvas.add(cell.getShape());
+            cells.add(newCell);
+            canvas.add(newCell.getShape());
+        }
+
+        
     }
 
     private static double sqr(double x) {
